8fa7ad19deda6260cb91c9934072ed0e770c8ffc
Jrazo <64182316+PAUSCC@users.noreply.github.com>
2022-08-25 08:09:56 -0700

    Create fix-finances.js

---
+++ Blog/fix-finances.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Thanks! Your response was successfully submitted! I will reach out shortly");
+      })
+    });
+  });
+
+  const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
