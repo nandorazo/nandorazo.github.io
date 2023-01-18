21c3ad40d4f03b20dbc71f97cb456918c523521f
Jrazo <64182316+PAUSCC@users.noreply.github.com>
2023-01-18 05:07:55 -0800

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Thanks! Your response was successfully submitted! I will reach out shortly");
+      })
+    });
+  });
